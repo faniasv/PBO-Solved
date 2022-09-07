49855d296f5aa6a0523cec16969279356fb5b828
Savilla Tifania Mahadewi <89194313+faniasv@users.noreply.github.com>
2022-09-07 21:37:13 +0700

    Create BangunRuang.java

---
+++ Praktikum2/BangunRuang/BangunRuang.java
@@ -0,0 +1,17 @@
+public class BangunRuang {
+    protected float luas;
+    protected float keliling;
+    protected float volume;
+
+    public void hitungLuas(){}
+
+    public void hitungKeliling(){}
+
+    public void hitungVolume(){}
+
+    public void printInfo(){
+        System.out.println("Luas        : "+this.luas);
+        System.out.println("Keliling    : "+this.keliling);
+        System.out.println("Volume      : "+this.volume);
+    }
+}
