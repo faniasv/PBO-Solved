bc629087e723514b31c2ccb9f67b5f59620d3c22
Savilla Tifania Mahadewi <89194313+faniasv@users.noreply.github.com>
2022-09-07 21:27:03 +0700

    Create Lingkaran.java

---
+++ Praktikum2/BangunDatar/Lingkaran.java
@@ -0,0 +1,19 @@
+public class Lingkaran extends BangunDatar {
+    private float phi;
+    private float r;
+
+    public Lingkaran(float xr){
+        this.phi = 22/7;
+        this.r = xr;
+    }
+
+    @Override
+    public void hitungLuas(){
+        this.luas = (this.phi*this.r*this.r);
+    }
+
+    @Override
+    public void hitungKeliling(){
+        this.keliling = (this.phi*2*this.r);
+    }
+}
