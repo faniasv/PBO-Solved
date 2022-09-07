e877693be6107f538368d2fa3e6c9c590a86a6f7
Savilla Tifania Mahadewi <89194313+faniasv@users.noreply.github.com>
2022-09-07 21:25:50 +0700

    Create BangunDatar.java

---
+++ Praktikum2/BangunDatar/BangunDatar.java
@@ -0,0 +1,15 @@
+public class BangunDatar {
+    protected float luas;
+    protected float keliling;
+
+    public BangunDatar(){}
+
+    public void hitungLuas(){}
+
+    public void hitungKeliling(){}
+
+    public void printInfo(){
+        System.out.println("Luas     :" +this.luas);
+        System.out.println("Keliling :" +this.keliling);
+    }
+}
