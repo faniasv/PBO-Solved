2af02c4e1beaaa5c10b55b2ddf00e2817921ad1b
Savilla Tifania Mahadewi <89194313+faniasv@users.noreply.github.com>
2022-10-16 00:41:19 +0700

    Update App.java

--- Praktikum3/Tugas/App.java
+++ Praktikum3/Tugas/App.java
@@ -17,5 +17,31 @@ public class App {
         jh.getTabungan().transfer(jn.getTabungan(), 20000);
         jn.toString();
         jh.toString();
+        
+        /*Membuat objek Bank */
+        Bank pens = new Bank();
+
+        /*Membuat objek Nasabah */
+        pens.tambahNasabah(1, "Kuning", "Elka");
+        pens.getNasabah(1).setTabungan(new Tabungan(250000));
+        pens.tambahNasabah(2, "Merah", "Elin");
+        pens.getNasabah(2).setTabungan(new Tabungan(100000));
+        pens.tambahNasabah(3, "Indigo", "MMB");
+        pens.getNasabah(3).setTabungan(new Tabungan(350000));
+        pens.tambahNasabah(4, "Magenta", "Sage");
+        pens.getNasabah(4).setTabungan(new Tabungan(400000));
+
+        /*Mendapat informasi jumlah Nasabah */
+        System.out.println("Jumlah Nasabah : " + pens.getJumlahNasabah());
+
+        /*Mendapat informasi Nasabah indeks ke-3 */
+        System.out.println("Nasabah indeks ke-3 : " + pens.getNasabah(3));
+
+        /*Mendapat informasi Nasabah namaAwal = Magenta dan namaAkhir = Sage */
+        pens.getNasabah("Nasabah namaAwal = Magenta dan namaAkhir = Sage"+"Magenta", "Sage");
+
+        /*Mendapat informasi Nasabah namaAwal */
+        pens.getNasabah("Nasabah namaAwal = Merah" + "Merah");
+        pens.getNasabah("Nasabah namaAwal = Kuning"+ "Kuning");
     }
 }
