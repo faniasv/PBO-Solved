5784f8767f6b8ba67432b5fc59171a20727e266a
Savilla Tifania Mahadewi <89194313+faniasv@users.noreply.github.com>
2022-11-02 21:46:38 +0700

    Create Scanner.java

---
+++ P9/Interface/Scanner.java
@@ -0,0 +1,25 @@
+package Interface;
+
+public interface Scanner{
+    public default void scaneImage(){
+        System.out.println("Scanning image...");
+    }
+
+    public interface Copier{
+        public void copyImage();
+    }
+
+    public class Printer implements Copier, Scanner{
+        public void scaneImage(){
+            System.out.println("Scanning image...");
+        }
+
+        public void copyImage(){
+            System.out.println("Copy image...");
+        }
+
+        public void printImage(){
+            System.out.println("Print image...");
+        }
+    }
+}
